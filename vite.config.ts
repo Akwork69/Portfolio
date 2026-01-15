912d2f05b5d23cead12be2d22f65b8a50b533a2c
Akwork69 <ak.work69@gmail.com>
2026-01-15 21:37:56 +0530

    initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "development" ? "/" : "/<Portfolio>/",
+  base: mode === "development" ? "/" : "/Portfolio/",
   server: {
     host: "::",
     port: 8080,
